917f4643e19de3dbda89cd9af8beff3ae380395b
adsiitam13 <36767537+adsiitam13@users.noreply.github.com>
2018-03-14 20:15:17 -0600

    Add files via upload

---
+++ MapReduce/src/main/java/MapReduce.java
@@ -0,0 +1,17 @@
+package mapreduce;
+
+import java.util.Collection;
+
+public class MapReduce{
+
+	public static void main (String args[]){
+		String[] nombres=new String[]
+	}
+
+}
+
+http://www.doublecloud.org/2014/04/java-8-new-features-map-reduce-made-easy-with-stream-apis/
+
+http://www.java67.com/2016/09/map-reduce-example-java8.html
+
+https://www.sitepoint.com/java-8-streams-filter-map-reduce/
